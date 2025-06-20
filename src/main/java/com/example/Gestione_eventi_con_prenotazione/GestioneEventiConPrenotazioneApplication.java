9be358418e414f6ad5a842c41017e029f02c32c5
11292-stella <stella.marucelli@gmail.com>
2025-06-20 09:56:54 +0200

    Inizio progetto Gestione Eventi

---
+++ src/main/java/com/example/Gestione_eventi_con_prenotazione/GestioneEventiConPrenotazioneApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gestione_eventi_con_prenotazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneEventiConPrenotazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneEventiConPrenotazioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Gestione_eventi_con_prenotazione/GestioneEventiConPrenotazioneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestione_eventi_con_prenotazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiConPrenotazioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
